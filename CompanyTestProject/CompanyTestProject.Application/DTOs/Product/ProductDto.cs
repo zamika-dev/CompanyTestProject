3bdfb7a17c562cb0d4b64f36f6b8b085c5d71dcc
Zamika <z.mhkia@gmail.com>
2023-12-17 23:13:41 +0330

    Add Dto Classes

---
+++ CompanyTestProject/CompanyTestProject.Application/DTOs/Product/ProductDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CompanyTestProject.Application.DTOs.Product
+{
+    public class ProductDto : BaseDto
+    {
+        public required string Name { get; set; }
+
+        public string? ManufactureEmail { get; set; }
+
+        public string? ManufacturePhone { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? ProduceDate { get; set; }
+
+        public bool IsAvailable { get; set; }
+    }
+}
---
+++ CompanyTestProject/CompanyTestProject.Application/DTOs/UserProductDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CompanyTestProject.Application.DTOs
+{
+    internal class UserProductDto : BaseDto
+    {
+        public required string UserId { get; set; }
+
+        public required int ProductId { get; set; }
+    }
+}
