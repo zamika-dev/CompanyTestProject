91de02913ec058016e2b35060944be7baa831f77
Zamika <z.mhkia@gmail.com>
2024-03-23 17:54:31 +0330

    Revert "add Category and ProductCategory to DomainClasses and packages of application layer"
    This reverts commit 152ab815532ec0f74236d2232b696dab4070fa35.

--- TagHelperSamples/TagHelperSamples/Program.cs
+++
@@ -1,8 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-app.MapDefaultControllerRoute();
-app.UseStaticFiles();
-
-app.Run();
