152ab815532ec0f74236d2232b696dab4070fa35
Zamika <z.mhkia@gmail.com>
2024-03-23 17:34:46 +0330

    add Category and ProductCategory to DomainClasses and packages of application layer

---
+++ TagHelperSamples/TagHelperSamples/TagHelpers/TableTagHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TagHelperSamples.TagHelpers
+{
+    [HtmlTargetElement(Attributes = "zm-tbl-attribute")]
+    public class TableTagHelper : TagHelper
+    {
+        public string ZmTblAttribute { get; set; } = "primary";
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.Add("class", $"table table-bordered table-striped table-{ZmTblAttribute}");
+            output.PostContent.Append("this is post content");
+        }
+    }
+}
